75f0156957c9db0665362053da63b875d344d6e0
edoardo0707 <142269889+edoardo0707@users.noreply.github.com>
2025-06-02 10:44:52 +0200

    Update sexList.js

--- sexList.js
+++ sexList.js
@@ -1,16 +1,3 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registrato con successo: ', registration);
-      })
-      .catch((error) => {
-        console.log('Errore nella registrazione del Service Worker: ', error);
-      });
-  });
-}
-
-
 
 document.getElementById("send-button").addEventListener("mouseup", function() {
       this.blur();
